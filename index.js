c2cf5cd1ac5317c17c1a2587fa35cf904cd9581c
Sandra Molina Palencia <smolinapalencia@gmail.com>
2022-03-06 19:04:05 +0000

    working on maps

--- index.js
+++ index.js
@@ -8,3 +8,11 @@ var swiper = new Swiper(".mySwiper", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+function changeSize() {
+  var frame = document.getElementById("frame");
+  var content = frame.contentDocument || frame.contentWindow;
+  content.body.style.fontSize = "1150%";
+}
+
+changeSize();
