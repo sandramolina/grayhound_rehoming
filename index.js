82158cefd17ef9a47373497caaba3f7c90510aca
Sandra Molina Palencia <smolinapalencia@gmail.com>
2022-03-12 16:43:46 +0000

    Improves how the site looks

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-// var swiper = new Swiper(".mySwiper", {
-//   pagination: {
-//     el: ".swiper-pagination",
-//     type: "fraction",
-//   },
-//   navigation: {
-//     nextEl: ".swiper-button-next",
-//     prevEl: ".swiper-button-prev",
-//   },
-// });
-
 function changeSize() {
   var frame = document.getElementById("frame");
   var content = frame.contentDocument || frame.contentWindow;
