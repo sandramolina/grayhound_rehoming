2c985c29c038ce31e1e25a302d83854b24ad6fe3
Sandra Molina Palencia <smolinapalencia@gmail.com>
2022-03-13 11:24:04 +0000

    Adds doggies array

--- quiz.js
+++ quiz.js
@@ -1,4 +1,35 @@
+// This function will save the user answers to the 4 questions into an array
 function answerInput(id) {
-  var input = document.getElementById(id).value;
+  let input = document.getElementById(id).value;
   console.log(input);
 }
+
+// this is the array that contains each of the dogs objects: children friendly, social dog, aged dog, female. Each boolean will tell if the dog meets these criteria
+let doggies = [
+  {
+    dog: "Delilah",
+    answerTag: [true, true, false, true],
+  },
+  {
+    dog: "Alfie",
+    answerTag: [true, false, true, false],
+  },
+  {
+    dog: "Lola",
+    answerTag: [true, true, false, true],
+  },
+  {
+    dog: "Sky",
+    answerTag: [false, true, false, false],
+  },
+  {
+    dog: "TBD1",
+    answerTag: [true, false, true, true],
+  },
+  {
+    dog: "TBD2",
+    answerTag: [true, true, false, false],
+  },
+];
+console.log(doggies[0].answerTag[2]); //false
+console.log(doggies[2]);
